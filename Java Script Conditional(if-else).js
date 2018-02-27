bd7c3b7b2e5ca6fdb4fbe081741de43263856399
Renal Apriansyah <renal>
2018-02-27 11:39:21 +0700

    memperbaharui agar nama bisa di input dengan nama siapa saja

--- Java Script Conditional(if-else).js
+++ Java Script Conditional(if-else).js
@@ -3,23 +3,24 @@ if(nama == ''){
 	console.log('Nama harus diisi!');
 }
 //Output untuk Input nama = 'Mikael' dan peran = ''
-else if(nama == 'Mikael' && peran == ''){
+else if( peran == ''){
 	console.log('Halo ' + nama + ' , Pilih peranmu untuk memulai game!');
 }
-//Output untuk Input nama = 'Nina' dan peran 'Ksatria'
-else if (nama == 'Nina' && peran == 'Ksatria'){
+//Output untuk Input peran 'Ksatria'
+else if (peran == 'Ksatria'){
 	console.log('Selamat datang di Dunia Proxytia, ' + nama);
 	console.log('Halo ' + peran + ' ' + nama +', kamu dapat menyerang dengan senjatamu!')
 }
-//Output untuk Input nama = 'Danu' dan peran 'Tabib'
-else if (nama == 'Danu' && peran == 'Tabib') {
+//Output untuk Input peran 'Tabib'
+else if (peran == 'Tabib') {
 	console.log('Selamat datang di Dunia Proxytia, ' + nama );
 	console.log('Halo ' + peran + ' ' + nama + ' , kamu akan membantu temanmu yang terluka.')
 }
-//Output untuk Input nama = 'Zero' dan peran 'Penyihir'
-else if (nama == 'Zero' && peran == 'Penyihir'){
+//Output untuk Input peran 'Penyihir'
+else if (peran == 'Penyihir'){
 	console.log('Selamat datang di Dunia Proxytia, ' + nama);
 	console.log('Halo ' + peran + ' '+ nama + ' , ciptakan keajaiban yang membantu kemenanganmu!')
 }
-
-
+else {
+	console.log('Maaf peran tidak tersedia, silahkan mengisi peran yang sudah tersedia')
+}
