722cdb914a4e42c045d28a9766721156b6fd68f3
Renal Apriansyah <renal>
2018-02-28 14:18:23 +0700

    add Thursday Task

---
+++ Java Script Function.js
@@ -0,0 +1,30 @@
+//Tugas 1
+function shoutOut(){
+	console.log('Halo Function!')
+}
+
+console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+
+//Tugas 2
+function calculateMultiply(num1,num2){
+	return num1 * num2
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
+
+//Tugas 3
+function processSentence(nama,umur,alamat,hobi){
+	console.log('Nama saya ' + nama + ', umur saya ' + umur + ' tahun, alamat saya di ' + alamat + ', dan saya punya hobby yaitu ' + hobi)
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
